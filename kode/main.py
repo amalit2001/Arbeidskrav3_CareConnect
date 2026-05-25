076bd748ba76a2192dec2d2213faddac0622bcec
amalit2001 <amalit@afk.no>
2026-05-25 03:08:28 +0200

    Ryddet opp i feil i main.py

--- kode/main.py
+++ kode/main.py
@@ -2,87 +2,6 @@ from bruker import Innbygger, Helsepersonell, Saksbehandler, Administrator
 from journal import Pasientjournal, TilgangAvslåttError
 from tilgang import Tilgangslogg, Tilgangskontroll
 
-    @property
-    def bruker_id(self) -> int:
-        return self.__bruker_id
-
-    @property
-    def navn(self) -> str:
-        return self.__navn
-
-    @property
-    def epost(self) -> str:
-        return self.__epost
-
-    @property
-    def rolle(self) -> Rolle:
-        return self.__rolle
-
-    def logg_inn(self) -> bool:
-        print(f"{self.__navn} har logget inn.")
-        return True
-
-    def logg_ut(self) -> None:
-        print(f"{self.__navn} har logget ut.")
-
-    @abstractmethod
-    def har_tilgang(self, ressurs: str, eier_id: int | None = None) -> bool: 
-       if ressurs == "pasientjournal":
-            return eier_id == self.bruker_id
-            return False
-       #Innbygger får lese egen journal, men ikke andre ressurser
-    """ Dette er en polymorf metode som gjør at underklassene selv bestemmer
-        hva de har tilgang til."""
-pass
-
-    @property
-    def journal_id(self) -> int:
-        return self.__journal_id
-
-    @property
-    def eier_id(self) -> int:
-        return self.__eier_id
-
-    def hent_journal(
-        self,
-        bruker: Bruker,
-        tilgangskontroll: Tilgangskontroll
-    ) -> str:
-        tilgang = tilgangskontroll.sjekk_tilgang(
-            bruker=bruker,
-            ressurs="pasientjournal",
-            eier_id=self.__eier_id,
-            handling="hent_journal"
-        )
-
-        if not tilgang:
-            raise TilgangAvslåttError(
-                f"Tilgang avslått: {bruker.navn} har ikke tilgang til journal {self.__journal_id}."
-            )
-
-        return self.__helseopplysninger
-
-    def legg_til_notat(
-        self,
-        bruker: Bruker,
-        notat: str,
-        tilgangskontroll: Tilgangskontroll
-    ) -> None:
-        tilgang = tilgangskontroll.sjekk_tilgang(
-            bruker=bruker,
-            ressurs="journal_notat",
-            eier_id=self.__eier_id,
-            handling="legg_til_notat"
-        )
-
-        if not tilgang:
-            raise TilgangAvslåttError(
-                f"Tilgang avslått: {bruker.navn} har ikke tilgang til journal {self.__journal_id}."
-            )
-
-        self.__notater.append(notat)
-
-
 def vis_logger(tilgangslogg: Tilgangslogg) -> None:
     print("\n--- Tilgangslogg ---")
     for logg in tilgangslogg.hent_logger():
@@ -94,7 +13,6 @@ def vis_logger(tilgangslogg: Tilgangslogg) -> None:
             f"handling={logg.handling} | "
             f"resultat={logg.resultat}"
         )
-
 
 def main() -> None:
     tilgangslogg = Tilgangslogg()
